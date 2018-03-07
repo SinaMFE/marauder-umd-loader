a62ed9c10dd7fac61fbfff89c292cb42665cff0f
fengwan <fengwan@staff.sina.com.cn>
2018-03-07 20:05:32 +0800

    支持onlineUrl

--- src/index.js
+++ src/index.js
@@ -15,19 +15,26 @@ function getPackageJson(url){
 export default function loader(source) {
     var me=this;
     var contextPath = this.context;
+    const options = loaderUtils.getOptions(this) || {};
     var pj = getPackageJson(contextPath);
-    if(pj.name.indexOf("@mfelibs")>=0){
-        var moduleName = pj.name.split("/")[1];
-        if(me._compilation["umdPath"]==null){
-            me._compilation["umdPath"]={};
-        }
-        me._compilation["umdPath"][moduleName]={
-            entry:"https://mjs.sinaimg.cn/umd/"+moduleName+"/"+pj.version+"/index.all.min.js"
-        }
+    var inject = options.inject||"body";
+    if (me._compilation["umdPath"] == null) {
+        me._compilation["umdPath"] = {
+            body:{},head:{}
+        };
     }
-    else{
+    if(options.onlineUrl!=null){
+        me._compilation["umdPath"][inject][options.onlineUrl] = {
+            entry: options.onlineUrl
+        };
+    }
+    else if (pj.name.indexOf("@mfelibs") >= 0) {
+        var moduleName = pj.name.split("/")[1];
+        me._compilation["umdPath"][inject][moduleName] = {
+            entry: "https://mjs.sinaimg.cn/umd/" + moduleName + "/" + pj.version + "/index.all.min.js"
+        };
+    } else {
         throw "不支持非@mfelibs域下的组件";
     }
-
     return "";
 }
