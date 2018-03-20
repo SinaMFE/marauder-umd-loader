9d62bed2a7ff8bfefe74c5eeb1535f3631e97227
小飞侠 <shixi_huyue3@staff.sina.com.cn>
2018-03-20 19:41:08 +0800

    v0.0.4

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+  value: true
 });
 exports.default = loader;
 
@@ -21,37 +21,66 @@ var _schemaUtils = require("schema-utils");
 
 var _schemaUtils2 = _interopRequireDefault(_schemaUtils);
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj };
+}
 
 function getPackageJson(url) {
-    if (!_fs2.default.existsSync(_path2.default.join(url, "./package.json"))) {
-        return getPackageJson(_path2.default.dirname(url));
-    } else {
-        return require(_path2.default.join(url, "./package.json"));
-    }
+  if (!_fs2.default.existsSync(_path2.default.join(url, "./package.json"))) {
+    return getPackageJson(_path2.default.dirname(url));
+  } else {
+    return require(_path2.default.join(url, "./package.json"));
+  }
 }
 function loader(source) {
-    var me = this;
-    var contextPath = this.context;
-    var options = _loaderUtils2.default.getOptions(this) || {};
-    var pj = getPackageJson(contextPath);
-    var inject = options.inject || "body";
-    if (me._compilation.compiler["umdPath"] == null) {
-        me._compilation.compiler["umdPath"] = {
-            body: {}, head: {}
-        };
-    }
-    if (options.onlineUrl != null) {
-        me._compilation.compiler["umdPath"][inject][options.onlineUrl] = {
-            entry: options.onlineUrl
-        };
-    } else if (pj.name.indexOf("@mfelibs") >= 0) {
-        var moduleName = pj.name.split("/")[1];
-        me._compilation.compiler["umdPath"][inject][moduleName] = {
-            entry: "https://mjs.sinaimg.cn/umd/" + moduleName + "/" + pj.version + "/index.all.min.js"
-        };
-    } else {
-        throw "不支持非@mfelibs域下的组件";
-    }
+  let me = this;
+
+  let options = _loaderUtils2.default.getOptions(this) || {};
+
+  let type = options.type ? options.type : "js";
+  let inject = options.inject ? options.inject : "body";
+  let onlineUrl = options.onlineUrl;
+
+  if (!onlineUrl) {
     return "";
+  }
+
+  if (!this._compiler.options.injectLink) {
+    this._compiler.options.injectLink = [];
+  }
+
+  this._compiler.options.injectLink.push({
+    type,
+    inject,
+    onlineUrl
+  });
+
+  //   var contextPath = this.context;
+  //   var pj = getPackageJson(contextPath);
+  //   var inject = options.inject || "body";
+
+  //   if (me._compilation.compiler["umdPath"] == null) {
+  //     me._compilation.compiler["umdPath"] = {
+  //       body: {},
+  //       head: {}
+  //     };
+  //   }
+  //   if (options.onlineUrl != null) {
+  //     me._compilation.compiler["umdPath"][inject][options.onlineUrl] = {
+  //       entry: options.onlineUrl
+  //     };
+  //   } else if (pj.name.indexOf("@mfelibs") >= 0) {
+  //     var moduleName = pj.name.split("/")[1];
+  //     me._compilation.compiler["umdPath"][inject][moduleName] = {
+  //       entry:
+  //         "https://mjs.sinaimg.cn/umd/" +
+  //         moduleName +
+  //         "/" +
+  //         pj.version +
+  //         "/index.all.min.js"
+  //     };
+  //   } else {
+  //     throw "不支持非@mfelibs域下的组件";
+  //   }
+  return "";
 }
